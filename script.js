e0a3bd9437d6abc79c045f513d4d014c6097cfcf
ReyHaan <hodekarehan@gmail.com>
2022-03-08 22:20:58 +0530

    added mobile nav

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-const hamburger = document.querySelector('.hamburger');
-const mobileNav = document.querySelector('.mobile-nav');
 
-hamburger.addEventListener('click', showMobileNav);
 
-function showMobileNav() {
-    hamburger.classList.toggle('open');
-    mobileNav.classList.toggle('open');
-}
+const nav = document.querySelector('#navigation');
+const navToggle = document.querySelector('.nav-toggle');
 
-// close menu when click on nav links
-const navLink = document.querySelectorAll(".nav-link");
+// when someone clicks the hamburger button/menu
+navToggle.addEventListener('click', () =>{
+    const visibility = nav.getAttribute('data-visible');
+    // if the menu is closed, open it
+    if(visibility === 'false') {
+        nav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    } else {
+        // if the menu is open, close it
+        nav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+    console.log(navToggle.getAttribute('aria-expanded'));
+})
 
-function closeMenu() {
-    hamburger.classList.remove("open");
-    mobileNav.classList.remove("open");
-}
